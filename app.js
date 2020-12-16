36265bb8a662f94c0a07d7c708b21a6f6ff3721e
shashank m.g <52480486+shashank-mg@users.noreply.github.com>
2020-12-16 21:36:42 +0530

    digital clock

---
+++ app.js
@@ -0,0 +1,58 @@
+let hours = document.querySelector(".hour");
+let minutes = document.querySelector(".minute");
+let seconds = document.querySelector(".second");
+
+class Clock {
+  constructor(hour_time, minute_time, second_time) {
+    this.hour_time = hour_time;
+    this.minute_time = minute_time;
+    this.second_time = second_time;
+  }
+
+  hours() {
+    let getHours = this.date.getHours();
+    if (getHours > 12) {
+      if (getHours.length < 2) {
+        this.hour_time.textContent = "0" + getHours;
+      } else {
+        this.hour_time.textContent = "0" + (getHours - 12);
+      }
+    } else {
+      if (getHours.length < 2) {
+        this.hour_time.textContent = "0" + getHours;
+      } else {
+        this.hour_time.textContent = getHours;
+      }
+    }
+  }
+
+  minutes() {
+    let getMinutes = this.date.getMinutes();
+    if (getMinutes <= 9) {
+      this.minute_time.textContent = "0" + getMinutes;
+    } else {
+      this.minute_time.textContent = getMinutes;
+    }
+  }
+
+  seconds() {
+    let getSeconds = this.date.getSeconds();
+    if (getSeconds <= 9) {
+      this.second_time.textContent = "0" + getSeconds;
+    } else {
+      this.second_time.textContent = getSeconds;
+    }
+  }
+
+  showTime() {
+    setInterval(() => {
+      this.date = new Date();
+      this.hours();
+      this.minutes();
+      this.seconds();
+    }, 1000);
+  }
+}
+
+let alaramClock = new Clock(hours, minutes, seconds);
+alaramClock.showTime();
