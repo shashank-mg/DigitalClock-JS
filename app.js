4e397ecb39e1e5a10d7a09df27e79df4549d0f4c
shashank m.g <52480486+shashank-mg@users.noreply.github.com>
2020-12-17 23:08:11 +0530

    alarm feature building

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
+let onOroff = document.createElement("div");
+let timeDisplay = document.querySelector(".time-display");
 let hours = document.querySelector(".hour");
 let minutes = document.querySelector(".minute");
 let seconds = document.querySelector(".second");
 let dayOrNight = document.querySelector(".dayOrNight");
+let time = document.querySelector(".time");
+let date = document.querySelector(".date");
+let alarm = document.querySelector(".set-alarm");
+let flag = 0;
 
 class Clock {
   constructor(hour_time, minute_time, second_time, day_night) {
@@ -9,15 +15,18 @@ class Clock {
     this.minute_time = minute_time;
     this.second_time = second_time;
     this.day_night = day_night;
+    this.time_display = timeDisplay;
+    this.on_off = onOroff;
   }
 
+  // Showing Time
   hours() {
     let getHours = this.date.getHours();
     if (getHours > 12) {
       if (getHours.length < 2) {
         this.hour_time.textContent = "0" + getHours;
       } else {
-        this.hour_time.textContent = "0" + (getHours - 12);
+        this.hour_time.textContent = getHours - 12;
       }
     } else {
       if (getHours.length < 2) {
@@ -29,6 +38,7 @@ class Clock {
   }
 
   minutes() {
+    this.minute_time.classList.remove("month");
     let getMinutes = this.date.getMinutes();
     if (getMinutes <= 9) {
       this.minute_time.textContent = "0" + getMinutes;
@@ -47,8 +57,9 @@ class Clock {
   }
 
   dayOrNight() {
+    // this.day_night.classList.remove("dayOrNight2");
     let getHours = this.date.getHours();
-    if (getHours > 12) {
+    if (getHours >= 12) {
       this.day_night.textContent = "PM";
     } else {
       this.day_night.textContent = "AM";
@@ -56,7 +67,19 @@ class Clock {
   }
 
   showTime() {
-    setInterval(() => {
+    this.day_night.classList.remove("dayOrNight2");
+    this.minute_time.classList.remove("minute2");
+    this.second_time.classList.remove("second2");
+    this.hour_time.classList.remove("hour2");
+
+    flag = 0;
+    this.date = new Date();
+    this.hours();
+    this.minutes();
+    this.seconds();
+    this.dayOrNight();
+
+    this.stopTime = setInterval(() => {
       this.date = new Date();
       this.hours();
       this.minutes();
@@ -64,7 +87,55 @@ class Clock {
       this.dayOrNight();
     }, 1000);
   }
+
+  // Show Date
+  displayDate() {
+    this.day_night.classList.remove("dayOrNight2");
+    this.minute_time.classList.remove("minute2");
+    this.second_time.classList.remove("second2");
+    this.hour_time.classList.remove("hour2");
+    flag = 1;
+
+    clearInterval(this.stopTime);
+    this.date = new Date();
+    this.hour_time.textContent = this.date.getDate();
+    this.minute_time.textContent = months[this.date.getMonth() - 1];
+    this.minute_time.classList.add("month");
+
+    this.second_time.textContent = this.date.getFullYear();
+    this.day_night.textContent = days[this.date.getDay() - 1];
+  }
+
+  // Set Alarm
+  setAlarm() {
+    this.time_display.appendChild(this.on_off);
+    this.minute_time.classList.remove("month");
+
+    this.day_night.classList.add("dayOrNight2");
+    this.minute_time.classList.add("minute2");
+    this.second_time.classList.add("second2");
+    this.hour_time.classList.add("hour2");
+
+    clearInterval(this.stopTime);
+    flag = 1;
+
+    this.hour_time.innerHTML = choose_hour;
+    this.minute_time.innerHTML = choose_minute;
+    this.second_time.innerHTML = choose_second;
+    this.day_night.innerHTML = amOrPm;
+    this.on_off.innerHTML = onOrOff;
+
+    this.on_off.style.marginRight = "10px";
+  }
 }
 
 let alaramClock = new Clock(hours, minutes, seconds, dayOrNight);
 alaramClock.showTime();
+
+time.addEventListener("click", () => {
+  if (flag === 1) alaramClock.showTime();
+});
+
+date.addEventListener("click", () => alaramClock.displayDate());
+
+alarm.addEventListener("click", () => alaramClock.setAlarm());
---
+++ elements.js
@@ -0,0 +1,209 @@
+let months = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+
+let days = [
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+  "Sunday",
+];
+
+let choose_hour = `
+    <div class="input-group mb-3">
+      <div class="input-group-append">
+      <label class="input-group-text" for="inputGroupSelect01">
+      Hours
+      </label>
+    </div>
+    <select class="custom-select" id="inputGroupSelect01">
+      <option selected>--:--</option>
+      <option value="1">1</option>
+      <option value="2">2</option>
+      <option value="3">3</option>
+      <option value="4">4</option>
+      <option value="5">5</option>
+      <option value="6">6</option>
+      <option value="7">7</option>
+      <option value="8">8</option>
+      <option value="9">9</option>
+      <option value="10">10</option>
+      <option value="11">11</option>
+      <option value="12">12</option>
+    </select>
+    </div>`;
+
+let choose_minute = `<div class="input-group mb-3">
+      <div class="input-group-append">
+      <label class="input-group-text" for="inputGroupSelect01">
+      Minute
+      </label>
+    </div>
+    <select class="custom-select" id="inputGroupSelect01">
+      <option selected>--:--</option>
+      <option value="1">1</option>
+      <option value="2">2</option>
+      <option value="3">3</option>
+      <option value="4">4</option>
+      <option value="5">5</option>
+      <option value="6">6</option>
+      <option value="7">7</option>
+      <option value="8">8</option>
+      <option value="9">9</option>
+      <option value="10">10</option>
+      <option value="11">11</option>
+      <option value="12">12</option>
+      <option value="13">13</option>
+      <option value="14">14</option>
+      <option value="15">15</option>
+      <option value="16">16</option>
+      <option value="17">17</option>
+      <option value="18">18</option>
+      <option value="19">19</option>
+      <option value="20">20</option>
+      <option value="21">21</option>
+      <option value="22">22</option>
+      <option value="23">23</option>
+      <option value="24">24</option>
+      <option value="25">25</option>
+      <option value="26">26</option>
+      <option value="27">27</option>
+      <option value="28">28</option>
+      <option value="29">29</option>
+      <option value="30">30</option>
+      <option value="31">31</option>
+      <option value="32">32</option>
+      <option value="33">33</option>
+      <option value="34">34</option>
+      <option value="35">35</option>
+      <option value="36">36</option>
+      <option value="37">37</option>
+      <option value="38">38</option>
+      <option value="39">39</option>
+      <option value="40">40</option>
+      <option value="41">41</option>
+      <option value="42">42</option>
+      <option value="43">43</option>
+      <option value="44">44</option>
+      <option value="45">45</option>
+      <option value="46">46</option>
+      <option value="47">47</option>
+      <option value="48">48</option>
+      <option value="49">49</option>
+      <option value="50">50</option>
+      <option value="51">51</option>
+      <option value="52">52</option>
+      <option value="53">53</option>
+      <option value="54">54</option>
+      <option value="55">55</option>
+      <option value="56">56</option>
+      <option value="57">57</option>
+      <option value="58">58</option>
+      <option value="59">59</option>
+    </select>
+    </div>`;
+
+let choose_second = `
+     <div class="input-group mb-3">
+      <div class="input-group-append">
+      <label class="input-group-text" for="inputGroupSelect01">
+      Second
+      </label>
+    </div>
+    <select class="custom-select" id="inputGroupSelect01">
+      <option selected>--:--</option>
+      <option value="1">1</option>
+      <option value="2">2</option>
+      <option value="3">3</option>
+      <option value="4">4</option>
+      <option value="5">5</option>
+      <option value="6">6</option>
+      <option value="7">7</option>
+      <option value="8">8</option>
+      <option value="9">9</option>
+      <option value="10">10</option>
+      <option value="11">11</option>
+      <option value="12">12</option>
+      <option value="13">13</option>
+      <option value="14">14</option>
+      <option value="15">15</option>
+      <option value="16">16</option>
+      <option value="17">17</option>
+      <option value="18">18</option>
+      <option value="19">19</option>
+      <option value="20">20</option>
+      <option value="21">21</option>
+      <option value="22">22</option>
+      <option value="23">23</option>
+      <option value="24">24</option>
+      <option value="25">25</option>
+      <option value="26">26</option>
+      <option value="27">27</option>
+      <option value="28">28</option>
+      <option value="29">29</option>
+      <option value="30">30</option>
+      <option value="31">31</option>
+      <option value="32">32</option>
+      <option value="33">33</option>
+      <option value="34">34</option>
+      <option value="35">35</option>
+      <option value="36">36</option>
+      <option value="37">37</option>
+      <option value="38">38</option>
+      <option value="39">39</option>
+      <option value="40">40</option>
+      <option value="41">41</option>
+      <option value="42">42</option>
+      <option value="43">43</option>
+      <option value="44">44</option>
+      <option value="45">45</option>
+      <option value="46">46</option>
+      <option value="47">47</option>
+      <option value="48">48</option>
+      <option value="49">49</option>
+      <option value="50">50</option>
+      <option value="51">51</option>
+      <option value="52">52</option>
+      <option value="53">53</option>
+      <option value="54">54</option>
+      <option value="55">55</option>
+      <option value="56">56</option>
+      <option value="57">57</option>
+      <option value="58">58</option>
+      <option value="59">59</option>
+    </select>
+    </div>`;
+
+let onOrOff = `
+    <label class="switch">
+      <input type="checkbox">
+      <span class="slider round"></span>
+    </label>
+    `;
+
+let amOrPm = `<div class="input-group mb-3">
+      <div class="input-group-append">
+      <label class="input-group-text" for="inputGroupSelect01">
+      AM/PM
+      </label>
+    </div>
+    <select class="custom-select" id="inputGroupSelect01">
+      <option selected>--:--</option>
+      <option value="AM">AM</option>
+      <option value="PM">PM</option>
+    </select>
+    </div>`;
