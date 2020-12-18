1f36b159dc0ce6d60b3cddd5408c46cbf14b977c
shashank m.g <52480486+shashank-mg@users.noreply.github.com>
2020-12-18 20:22:17 +0530

    refactoring

--- elements.js
+++ elements.js
@@ -7,7 +7,25 @@ let time = document.querySelector(".time");
 let date = document.querySelector(".date");
 let alarm = document.querySelector(".set-alarm");
 let flag = 0;
+let i = 1;
+let j = 1;
+let count = "";
+let a_count = "";
+let num = "";
 
+while (i < 60) {
+  if (String(i).length < 2) num = "0" + i;
+  else num = i;
+  while (j < 13) {
+    a_count += ` <option value="${num}">${num}</option> `;
+    j++;
+    break;
+  }
+  count += ` <option value="${num}">${num}</option> `;
+  num = "";
+  i++;
+}
+console.log(a_count);
 let months = [
   "January",
   "February",
@@ -33,7 +51,8 @@ let days = [
   "Sunday",
 ];
 
-let choose_hour = `
+let choose_hour =
+  `
     <div class="input-group mb-3">
       <div class="input-group-append">
       <label class="input-group-text" for="inputGroupSelect01">
@@ -42,161 +61,38 @@ let choose_hour = `
     </div>
     <select class="custom-select-hour" id="inputGroupSelect01">
       <option selected value='00'>00</option>
-      <option value="01">01</option>
-      <option value="02">02</option>
-      <option value="03">03</option>
-      <option value="04">04</option>
-      <option value="05">05</option>
-      <option value="06">06</option>
-      <option value="07">07</option>
-      <option value="08">08</option>
-      <option value="09">09</option>
-      <option value="10">10</option>
-      <option value="11">11</option>
-      <option value="12">12</option>
-    </select>
+      ` +
+  a_count +
+  ` </select>
     </div>`;
 
-let choose_minute = `<div class="input-group mb-3">
+let choose_minute =
+  `<div class="input-group mb-3">
       <div class="input-group-append">
       <label class="input-group-text" for="inputGroupSelect01">
       Minute
       </label>
     </div>
-    <select class="custom-select-minute" id="inputGroupSelect01">
+      <select class="custom-select-minute" id="inputGroupSelect01">
       <option value="00" selected>00</option>
-      <option value="01">01</option>
-      <option value="02">02</option>
-      <option value="03">03</option>
-      <option value="04">04</option>
-      <option value="05">05</option>
-      <option value="06">06</option>
-      <option value="07">07</option>
-      <option value="08">08</option>
-      <option value="09">09</option>
-      <option value="10">10</option>
-      <option value="11">11</option>
-      <option value="12">12</option>
-      <option value="13">13</option>
-      <option value="14">14</option>
-      <option value="15">15</option>
-      <option value="16">16</option>
-      <option value="17">17</option>
-      <option value="18">18</option>
-      <option value="19">19</option>
-      <option value="20">20</option>
-      <option value="21">21</option>
-      <option value="22">22</option>
-      <option value="23">23</option>
-      <option value="24">24</option>
-      <option value="25">25</option>
-      <option value="26">26</option>
-      <option value="27">27</option>
-      <option value="28">28</option>
-      <option value="29">29</option>
-      <option value="30">30</option>
-      <option value="31">31</option>
-      <option value="32">32</option>
-      <option value="33">33</option>
-      <option value="34">34</option>
-      <option value="35">35</option>
-      <option value="36">36</option>
-      <option value="37">37</option>
-      <option value="38">38</option>
-      <option value="39">39</option>
-      <option value="40">40</option>
-      <option value="41">41</option>
-      <option value="42">42</option>
-      <option value="43">43</option>
-      <option value="44">44</option>
-      <option value="45">45</option>
-      <option value="46">46</option>
-      <option value="47">47</option>
-      <option value="48">48</option>
-      <option value="49">49</option>
-      <option value="50">50</option>
-      <option value="51">51</option>
-      <option value="52">52</option>
-      <option value="53">53</option>
-      <option value="54">54</option>
-      <option value="55">55</option>
-      <option value="56">56</option>
-      <option value="57">57</option>
-      <option value="58">58</option>
-      <option value="59">59</option>
-    </select>
+      ` +
+  count +
+  `</select>
     </div>`;
 
-let choose_second = `
-     <div class="input-group mb-3">
-      <div class="input-group-append">
-      <label class="input-group-text" for="inputGroupSelect01">
-      Second
-      </label>
+let choose_second =
+  ` <div class="input-group mb-3">
+    <div class="input-group-append">
+    <label class="input-group-text" for="inputGroupSelect01">
+     Second
+    </label>
     </div>
     <select class="custom-select-second" id="inputGroupSelect01">
-      <option value="00" selected>00</option>
-      <option value="01">01</option>
-      <option value="02">02</option>
-      <option value="03">03</option>
-      <option value="04">04</option>
-      <option value="05">05</option>
-      <option value="06">06</option>
-      <option value="07">07</option>
-      <option value="08">08</option>
-      <option value="09">09</option>
-      <option value="10">10</option>
-      <option value="11">11</option>
-      <option value="12">12</option>
-      <option value="13">13</option>
-      <option value="14">14</option>
-      <option value="15">15</option>
-      <option value="16">16</option>
-      <option value="17">17</option>
-      <option value="18">18</option>
-      <option value="19">19</option>
-      <option value="20">20</option>
-      <option value="21">21</option>
-      <option value="22">22</option>
-      <option value="23">23</option>
-      <option value="24">24</option>
-      <option value="25">25</option>
-      <option value="26">26</option>
-      <option value="27">27</option>
-      <option value="28">28</option>
-      <option value="29">29</option>
-      <option value="30">30</option>
-      <option value="31">31</option>
-      <option value="32">32</option>
-      <option value="33">33</option>
-      <option value="34">34</option>
-      <option value="35">35</option>
-      <option value="36">36</option>
-      <option value="37">37</option>
-      <option value="38">38</option>
-      <option value="39">39</option>
-      <option value="40">40</option>
-      <option value="41">41</option>
-      <option value="42">42</option>
-      <option value="43">43</option>
-      <option value="44">44</option>
-      <option value="45">45</option>
-      <option value="46">46</option>
-      <option value="47">47</option>
-      <option value="48">48</option>
-      <option value="49">49</option>
-      <option value="50">50</option>
-      <option value="51">51</option>
-      <option value="52">52</option>
-      <option value="53">53</option>
-      <option value="54">54</option>
-      <option value="55">55</option>
-      <option value="56">56</option>
-      <option value="57">57</option>
-      <option value="58">58</option>
-      <option value="59">59</option>
-    </select>
-    </div>`;
+    <option value="00" selected>00</option>
+  ` +
+  count +
+  `</select>
+  </div>`;
 
 let onOrOff = `
   <input class='btn' type="button" name="" value="off">
